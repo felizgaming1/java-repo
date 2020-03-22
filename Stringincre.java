66ec0472e7acb3dab4bd38efbdd2c658fd70ed52
felizgaming1 <52193806+felizgaming1@users.noreply.github.com>
2020-03-22 16:35:29 +0530

    Create Stringincre.java
    done

---
+++ Stringincre.java
@@ -0,0 +1,42 @@
+public class Stringincre {
+
+    public static void solution(String s) {
+        int flag = 0;
+        int len = s.length();
+        int[] ans = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            if (Character.isDigit(s.charAt(i)) ) {
+                System.out.println("ERROR");
+                System.exit(0);
+            }
+            if (i == 0) {
+                ans[i] = ++flag;
+            }
+            for (int j = i; j >= 0; j--) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    ans[i] = ans[j];
+
+                }
+            }
+            if (ans[i] == 0) {
+                ans[i] = ++flag;
+            }
+
+        }
+        for (int i = 0; i < len; i++) {
+            System.out.print(ans[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "abcd";
+        String s1 ="abcadc";
+        String s2="na4";
+        solution(s);
+        System.out.println();
+        solution(s1);
+        System.out.println();
+        solution(s2);
+    }
+}
